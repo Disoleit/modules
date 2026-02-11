aeff3f7fa8e6b9a1f254bfcaedf3896aea72d038
Disoleit <sagatl1994@gmail.com>
2026-02-11 20:33:05 +0300

    first export

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    mode: 'production',
+    // entry: './src/js/index.js',
+    // output: {
+    //     path: path.resolve(__dirname, './dist'),
+    //     filename: 'app.bundle.js'
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCSSExtractPlugin()
+    ]
+}
